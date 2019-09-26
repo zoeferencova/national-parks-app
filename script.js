bfad7079d146ef4956364aacb79509dfed4a643e
zoeferencova <zoeferencova@gmail.com>
2019-09-26 15:46:45 -0400

    added conditional to address/image html to account for cases with no address/image

--- script.js
+++ script.js
@@ -15,11 +15,18 @@ function formatQuery(params) {
 
 function displayParks(response) {
 	emptyContents();
-	console.log(response.data[0])
 	$('#js-error-message').empty();
 	$('#results').prepend(`<h2>Search results</h2>`)
 	for (let i=0; i < response.data.length; i++) {
-		$('#result-list').append(`<li><h3 class="park-name">${response.data[i].fullName}</h3><p class="park-address">Address: ${response.data[i].addresses[1].line1}, ${response.data[i].addresses[1].city},  ${response.data[i].addresses[1].stateCode} ${response.data[i].addresses[1].postalCode}</p><img src="${response.data[i].images[0].url}" alt="${response.data[i].images[0].altText}" class="park-image"><p class="park-description">${response.data[i].description}</p><a href="${response.data[i].url}" target="_blank" class="park-website">Visit Website</a></li>`)
+		console.log(response.data[i].fullName)
+		$('#result-list').append(
+			`<li>
+			<h3 class="park-name">${response.data[i].fullName}</h3>
+			${response.data[i].addresses[1] === undefined ? '' : `<p class="park-address">Address: ${response.data[i].addresses[1].line1}, ${response.data[i].addresses[1].city},  ${response.data[i].addresses[1].stateCode} ${response.data[i].addresses[1].postalCode}</p>`}
+			${response.data[i].images[1] === undefined ? '' : `<img src="${response.data[i].images[0].url}" alt="${response.data[i].images[0].altText}" class="park-image">`}
+			<p class="park-description">${response.data[i].description}</p>
+			<a href="${response.data[i].url}" target="_blank" class="park-website">Visit Website</a>
+			</li>`)
 	}
 	$('#results').removeClass('hidden');
 }
